c17ed8dc30185d0da017d66bbeab68c365c24e48
Vagner Belfort <vagner.belfort@gria.io>
2021-10-16 12:47:52 -0300

    FIX: proper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,6 +33,9 @@ def go(args):
     df['last_review'] = pd.to_datetime(df['last_review'])
     logger.info("Transforming last_review column as date type.")
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     df.to_csv("clean_sample.csv")
     logger.info("Save dataset clean")
 
